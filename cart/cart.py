d53b54546480697e79d6e7b2081da79e0fb9a9ed
Eugene Lashko <yauhenilashko@gmail.com>
2022-03-09 19:03:16 +0300

    some repair

--- cart/cart.py
+++ cart/cart.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from shop.models import Product
 
 
-
 class Cart(object):
+
     def __init__(self, request):
         """
         Инициализация корзины
@@ -13,14 +13,16 @@ class Cart(object):
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
-            self.cart = cart
+        self.cart = cart
 
     def __iter__(self):
         """
-        Перебираем товары в корзине и получаем товары из базы данных
+        Перебираем товары в корзине и получаем товары из базы данных.
         """
         product_ids = self.cart.keys()
+        # получаем товары и добавляем их в корзину
         products = Product.objects.filter(id__in=product_ids)
+
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]['product'] = product
@@ -32,32 +34,32 @@ class Cart(object):
 
     def __len__(self):
         """
-        Считаем количество товаров в корзине
+        Считаем сколько товаров в корзине
         """
         return sum(item['quantity'] for item in self.cart.values())
 
     def add(self, product, quantity=1, update_quantity=False):
         """
-        Добавляем товар в корзину или обновляем его количество
+        Добавляем товар в корзину или обновляем его количество.
         """
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
         self.save()
- 
+
     def save(self):
-        self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
 
     def remove(self, product):
         """
         Удаляем товар
         """
-        product_id = product.id.objects.filter()
+        product_id = str(product.id)
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
@@ -67,29 +69,4 @@ class Cart(object):
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
-        self.session.modified = True # добавил
         self.save()
-# добавил все, что снизу
-
-    # def __iter__(self):
-    #     """
-    #     Iterate over the items in the cart and get the products
-    #     from the database.
-    #     """
-    #     product_ids = self.cart.keys()
-    #     # get the product objects and add them to the cart
-    #     products = Product.objects.filter(id__in=product_ids)
-    #     for product in products:
-    #         self.cart[str(product.id)]['product'] = product
-    #
-    #     for item in self.cart.values():
-    #         item['price'] = Decimal(item['price'])
-    #         item['total_price'] = item['price'] * item['quantity']
-    #         yield item
-
-
-    # def __len__(self):
-    #     """
-    #     Считаем количество товаров в корзине
-    #     """
-    #     return sum(item['quantity'] for item in self.cart.values())
